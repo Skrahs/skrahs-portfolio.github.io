e2c2c34ea987b556e0bad3aa1095c71b275d9ba4
Skrahs <108346039+Skrahs@users.noreply.github.com>
2024-08-22 21:48:33 +0200

    upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-toggle-btn').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
